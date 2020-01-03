8374bdf10d42bddebf47465d4d140570f9d32255
malli518 <malli.lead518@gmail.com>
2020-01-03 19:34:30 +0530

    updated tomcat dependency

--- src/main/java/com/rythmos/SampleApp.java
+++ src/main/java/com/rythmos/SampleApp.java
@@ -7,6 +7,8 @@ import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 import org.springframework.web.client.RestTemplate;	
 
@@ -16,12 +18,15 @@ import org.springframework.web.client.RestTemplate;
 		SampleApp.class,
 		Jsr310JpaConverters.class
 })
-public class SampleApp {
+public class SampleApp extends SpringBootServletInitializer {
 	@PostConstruct
 	void init() {
 		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 	}
-
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SampleApp.class);
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(SampleApp.class, args);
 		
